9bb9ddf8d9422e4ceddc699f8df7417f41283d75
Isara Madunika <99160274+isaramadunika@users.noreply.github.com>
2025-05-30 22:03:41 +0530

    Delete My_Portfolio directory

--- My_Portfolio/database/init_db.py
+++
@@ -1,8 +0,0 @@
-from database.models import Database
-
-def init_database():
-    db = Database()
-    print("Database initialized successfully!")
-
-if __name__ == "__main__":
-    init_database() 
